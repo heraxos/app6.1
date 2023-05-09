5cf17b5a0f9ed9425f223f8cf68a0a942be702c9
Armen <91148484+heraxos@users.noreply.github.com>
2023-05-09 17:01:53 +0300

    Create lib.php

---
+++ lib.php
@@ -0,0 +1,12 @@
+<?php 
+function connectToDB($user,$pass){
+    $db = new PDO('mysql:host=localhost;dbname=u47755', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    return $db;
+}
+function connectDB(){
+    $user = 'u47755';
+    $pass = '2914865';
+    $db = new PDO('mysql:host=localhost;dbname=u47551', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    return $db;
+}
+?>
